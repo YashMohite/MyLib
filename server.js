81a528698b4c425d929b8f9816f9f92b879d6461
YashMohite <supriyamohite83@gmail.com>
2020-12-06 16:52:20 +0530

    Initial Setup

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const expressLayouts = require("express-ejs-layouts")
+const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '\views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
@@ -14,8 +14,8 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/MyLib", { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log(`Connected To Mongoose`))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server Started On Port: 3000`))
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'))
